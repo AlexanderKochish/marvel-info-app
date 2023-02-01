e14c2baadb5c1ee8ac345050ab185790d9a9c3f8
Alex <103049907+AlexanderKochish@users.noreply.github.com>
2023-02-01 14:46:11 +0200

    form not found character message

--- src/components/searchForm/SearchCharForm.jsx
+++ src/components/searchForm/SearchCharForm.jsx
@@ -6,32 +6,34 @@ import './searchCharForm.scss'
 const SearchCharForm = ({getCharForName}) => {
     const[charName,setCharName] = useState('')
     const[foundChar,setFoundChar] = useState(null)
+    const[notFound,setNotFound] = useState('')
 
     const handleSub = (data) => getCharForName(data)
 
     const searchChar = async(e) => {
         e.preventDefault()
-        if(!charName.trim().length)  return 'This field is required'
+        if(charName.trim().length === 0) return setNotFound('This field is required')
         try {
             const res = await fetch(`${BASE_URL}characters?name=${charName}&${API_KEY}`)
             const {data} = await res.json()
+            if(!data.results.length) return setNotFound('The character was not found. Check the name and try again')
             handleSub(data.results[0])
-            return setFoundChar(data.results[0])
+            setFoundChar(data.results[0])
         } catch (error) {
             console.log(error.message);
-            return 'The character was not found. Check the name and try again'
         }  
     }
 
     const content = <div className='char__form-bottom__block'>
-        <p >There is! Visit {foundChar?.name} page?</p>
+        <p>There is! Visit {foundChar?.name} page?</p>
         <Link to={`/character/${foundChar?.id}`}>
         <button className="button button__secondary">
             <div className="inner">TO PAGE</div>
         </button>
         </Link>
     </div>
-    
+
+    const notFoundSearch = <div className='char__form-notfound'>{notFound}</div>
   return (
     <form onSubmit={searchChar} className='char__form'>
         <label>Or find a character by name:</label>
@@ -44,7 +46,7 @@ const SearchCharForm = ({getCharForName}) => {
         <button className="button button__main">
             <div className="inner">find</div>
         </button>
-        {!foundChar? '' : content}
+        {!foundChar? notFoundSearch : content}
     </form>
   )
 }
